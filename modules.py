b41fce7354c6fd4c90483f644ff9558521e69974
Joey <wangzhaoyu777@gmail.com>
2023-03-27 21:22:48 -0400

    add a test function schedule

--- modules.py
+++ modules.py
@@ -1,7 +1,19 @@
 # Zhaoyu Wang developed
 # time: 2023-03-26 11:10 a.m.
 import math
+import urllib.request
+import schedule
+import time
 
-print(dir(math))
-print(math.pi)
-print(math.pow(2, 4))
+# print(dir(math))
+# print(math.pi)
+# print(math.pow(2, 4))
+# print(urllib.request.urlopen('http://www.google.com').read)
+
+def job():
+    print('ok-----')
+schedule.every(3).seconds.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
