5395b86f652a6779bdba2211f43afc54a3cd0aa0
Joey <wangzhaoyu777@gmail.com>
2023-03-27 22:29:39 -0400

    add openfile write

---
+++ writefile.py
@@ -0,0 +1,11 @@
+# Zhaoyu Wang developed
+# time: 2023-03-27 9:52 p.m.
+
+# file = open('b.txt','w')
+file = open('b.txt','a')
+# file.write('xinxinxin')
+file.write('xinxinxin')
+file.close()
+
+
+
