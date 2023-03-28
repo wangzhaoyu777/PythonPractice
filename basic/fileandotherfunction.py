211700cfefc1b512707951eca6b69c5d6e217ac4
Joey <wangzhaoyu777@gmail.com>
2023-03-28 11:38:35 -0400

    practice search file with ending with py

--- basic/fileandotherfunction.py
+++ basic/fileandotherfunction.py
@@ -1,6 +1,6 @@
 # Zhaoyu Wang developed
 # time: 2023-03-27 9:52 p.m.
-
+import os
 # file = open('a.txt','r')
 # print(file.read(2))
 # file.close()
@@ -12,8 +12,18 @@
 # # file.writelines(lst)
 # file.close()
 # print('------------------------------------------')
-file = open('b.txt', 'r')
-file.seek(0)
-print(file.read())
-print(file.tell())
-file.close()
+# file = open('b.txt', 'r')
+# file.seek(0)
+# print(file.read())
+# print(file.tell())
+# file.close()
+# os.system('calc')
+# os.system('notepad')
+# os.startfile('C:\\steam\\steam.exe')
+path = os.getcwd()
+lst = os.listdir(path)
+for filename in lst:
+    if filename.endswith('.py'):
+        print(filename)
+
+
