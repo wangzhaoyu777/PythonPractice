92d238435f2fb630a6fd72a804befe828fc426d9
Joey <wangzhaoyu777@gmail.com>
2023-04-25 15:27:23 -0400

    reversal X and Y axis and move label to right

--- data-visualization/07_基础柱状图练习.py
+++ data-visualization/07_基础柱状图练习.py
@@ -1,12 +1,14 @@
 # Zhaoyu Wang developed
 # time: 2023-04-25 2:34 p.m.
 from pyecharts.charts import Bar
+from pyecharts.options import LabelOpts
 
 # 构建柱状图
 bar = Bar()
 # 添加X轴数据
 bar.add_xaxis(['中国','美国','英国'])
 # 添加Y轴数据
-bar.add_yaxis('GDP', ['30', '20', '10'])
+bar.add_yaxis('GDP', ['30', '20', '10'], label_opts=LabelOpts(position='right'))
+bar.reversal_axis()
 # render
 bar.render('基础柱状图.html')
